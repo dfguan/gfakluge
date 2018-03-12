4b64a528f50e2802dc22810f236a2b35eafbef69
dfguan <dfguan@hit.edu.cn>
2018-03-12 10:34:48 +0000

    add version info for gfa_to_fa

--- scripts/gfa_to_fa.py
+++ scripts/gfa_to_fa.py
@@ -7,9 +7,23 @@ if __name__ == "__main__":
         print "No GFA file specified, exit 1"
     else:
         fl = open(sys.argv[1])
-        for ln in fl:
-            ln_list = ln.strip('\n').split('\t')
-            if ln_list[0] == 'S':
-                print ">"+ln_list[1]
-                print ln_list[2]
-
+        #first line get version
+        frst_ln_list = fl.readline().strip('\n').split('\t')
+        vn = -1
+        for i in frst_ln_list:
+            if i[0:2] == "VN":
+                vn = float(i[5:])
+        if vn >= 1.0 and vn < 2.0:
+            for ln in fl:
+                ln_list = ln.strip('\n').split('\t')
+                if ln_list[0] == 'S':
+                    print ">"+ln_list[1]
+                    print ln_list[2]
+        elif vn >= 2.0:
+            for ln in fl:
+                ln_list = ln.strip('\n').split('\t')
+                if ln_list[0] == 'S':
+                    print ">"+ln_list[1]
+                    print ln_list[3]
+        else:
+            print "No version info found in GFA file, quit converting..."
