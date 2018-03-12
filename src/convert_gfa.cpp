a9dbdf1ea98f5c9e888b48ae780a08287e7ce077
dfguan <dfguan@hit.edu.cn>
2018-03-12 10:16:40 +0000

    add output_gfa_type

--- src/convert_gfa.cpp
+++ src/convert_gfa.cpp
@@ -42,6 +42,7 @@ int help()
 	fprintf(stderr, "Usage: convert_gfa [options] <GFA_FILE>\n\n");
 	fprintf(stderr, "options: -r FILE    required if a gfa file doesn't contain sequences\n");
 	fprintf(stderr, "         -s         output non-overlapping string graph\n");
+	fprintf(stderr, "         -g INT     output gfa type, 1 for GFA1, 2 for GFA 2 [1]");
 	fprintf(stderr, "         -h         print help information\n");
 	return NORMAL;
 }
@@ -64,13 +65,27 @@ int check_seq(GFAKluge &g)
 	}
 	return NORMAL;
 }
-int output_seqs(vector<sequence_elem> &ss)
+int output_seqs(vector<sequence_elem> &ss, int type)
 {
-	for (auto s : ss) cout << s.to_string_2() << endl; 
+	if (type == 1) {
+		for (auto s : ss) cout << s.to_string_1() << endl; 
+	} else {
+		for (auto s : ss) cout << s.to_string_2() << endl; 
+	}
 	ss.clear();
 	return NORMAL;
 }
 
+int output_edges(vector<edge_elem> &ee, int type)
+{
+	if (type == 1) {
+		for (auto e : ee) cout << e.to_string_1() << endl; 
+	} else {
+		for (auto e : ee) cout << e.to_string_2() << endl; 
+	}
+	ee.clear();
+	return NORMAL;
+}
 
 bool is_p_ctg(string g_id)
 {
@@ -83,73 +98,107 @@ string zeroPadNum(int n)
 	ss<<setw(7)<<setfill('0')<<n;
 	return ss.str();
 }
-int conv_p_2_s_s(GFAKluge& gg)
+int conv_p_2_s_s(GFAKluge& gg, int g_t)
 {
 	//convert to split sequence
+	if (g_t == 1) {
+		cout<<"H\tVN:Z:1.0"<<endl;
+	} else {
+		cout<<"H\tVN:Z:2.0"<<endl;
+	}
 	map<string, path_elem> pp = gg.get_name_to_path();		
 	map<string, vector<edge_elem>> n_2_e = gg.get_seq_to_edges();
 	map<string, sequence_elem, custom_key> n_2_s = gg.get_name_to_seq();
 	int count = 0;
 	for (auto it : pp) {
-		if (is_p_ctg(it.first)) {
-			vector<sequence_elem> ss;
-			sequence_elem s;
-			
-			path_elem &p = it.second;
-			string pre_seq_id = p.segment_names[0];
-			string cur_seq_id;
-			int cor_s, cor_e;
-			bool isRc, isExist;
+		vector<sequence_elem> ss;
+		vector<edge_elem> ee;
+		sequence_elem s;
+		edge_elem e;
 
-			s.name = "E"+zeroPadNum(count++); 
-			s.length = n_2_s[pre_seq_id].length;
-			s.sequence = n_2_s[pre_seq_id].sequence;
-			//opt_elem o = {"LN","i",to_string(s.length)};
-			//s.opt_fields.push_back(o);
-			opt_elem o = {"sn","Z",pre_seq_id};
-			s.opt_fields.push_back(o);
-			ss.push_back(s); // the first one
-		
-			for (size_t i = 1 ; i < p.segment_names.size(); ++i) {
-				isExist = false;
-				for (auto a : n_2_e[pre_seq_id]) {
-					if (a.sink_name == p.segment_names[i]) {
-						cor_s = stoi(a.tags["ob"].val);
-						cor_e = stoi(a.tags["oe"].val);
-						isRc	=	false;
-						isExist =	true;
-					}
+		path_elem &p = it.second;
+		string pre_seq_id = p.segment_names[0];
+		string cur_seq_id;
+		int cor_s, cor_e;
+		bool isRc, isExist;
+
+		s.name = "S"+zeroPadNum(count++); 
+		s.length = n_2_s[pre_seq_id].length;
+		s.sequence = p.orientations[0] ? n_2_s[pre_seq_id].sequence : rc_dna_seq(n_2_s[pre_seq_id].sequence, 0, n_2_s[pre_seq_id].length);
+		//opt_elem o = {"LN","i",to_string(s.length)};
+		//s.opt_fields.push_back(o);
+		opt_elem o = {"si","Z",pre_seq_id};
+		s.opt_fields.push_back(o);
+		ss.push_back(s); // the first one
+		e.type = 1;
+		e.source_name = s.name;		
+		e.source_orientation_forward = true;
+		e.source_begin = s.length;
+		e.source_end = s.length;
+		e.ends.set(0, 1);
+		e.ends.set(1, 1);
+
+		for (size_t i = 1 ; i < p.segment_names.size(); ++i) {
+			isExist = false;
+			for (auto a : n_2_e[pre_seq_id]) {
+				if (a.sink_name == p.segment_names[i]) {
+					cor_s = stoi(a.tags["ob"].val);
+					cor_e = stoi(a.tags["oe"].val);
+					isRc	=	false;
+					isExist =	true;
 				}
-				if (isExist) {
-					if (cor_s > cor_e) {
-						int t = cor_s;
-						cor_s = cor_e;
-						cor_e = t;
-						isRc = true;	
-					}	
-				} else 
-					return FL_FORMAT_ERR;
-				sequence_elem t;
-				t.name = "E"+zeroPadNum(count++); 
-				t.sequence =  (isRc ? rc_dna_seq(n_2_s[p.segment_names[i]].sequence, cor_s, cor_e) : n_2_s[p.segment_names[i]].sequence.substr(cor_s, cor_e - cor_s));  
-				t.length = cor_e - cor_s;
-				pre_seq_id = p.segment_names[i];
-				//o = {"LN","i",to_string(s.length)};
-				//t.opt_fields.push_back(o); 
-				o = {"sn","Z",pre_seq_id};
-				t.opt_fields.push_back(o); 
-				ss.push_back(t);
-				if (ss.size() > LIMITED_SIZE) 
-					output_seqs(ss);
 			}
-			if (ss.size()) 
-				output_seqs(ss);
+			if (isExist) {
+				if (cor_s > cor_e) {
+					int t = cor_s;
+					cor_s = cor_e;
+					cor_e = t;
+					isRc = true;	
+				}	
+			} else 
+				return FL_FORMAT_ERR;
+			sequence_elem t;
+			t.name = "S"+zeroPadNum(count++); 
+			t.sequence =  (isRc ? rc_dna_seq(n_2_s[p.segment_names[i]].sequence, cor_s, cor_e) : n_2_s[p.segment_names[i]].sequence.substr(cor_s, cor_e - cor_s));  
+			t.length = cor_e - cor_s;
+			e.sink_name = t.name;
+			e.sink_orientation_forward = true;
+			e.sink_begin = 0;
+			e.sink_end = 0;
+			e.ends.set(2, 0);
+			e.ends.set(3, 0);
+			e.alignment = "0M";
+			ee.push_back(e);
+			pre_seq_id = p.segment_names[i];
+			o = {"si","Z",pre_seq_id};
+			t.opt_fields.push_back(o); 
+			ss.push_back(t);
+			
+			//init a new line
+			e.type = 1;
+			e.source_name = t.name;		
+			e.source_orientation_forward = true;
+			e.source_begin = t.length;
+			e.source_end = t.length;
+			e.ends.set(0, 1);
+			e.ends.set(1, 1);
+
+			//o = {"LN","i",to_string(s.length)};
+			//t.opt_fields.push_back(o); 
+			if (ss.size() > LIMITED_SIZE) 
+				output_seqs(ss, g_t);
+			if (ee.size() > LIMITED_SIZE) 
+				output_edges(ee, g_t);
 		}
+		if (ss.size()) 
+			output_seqs(ss, g_t);
+		if (ee.size()) 
+			output_edges(ee, g_t);
 	}	
 	return NORMAL;
 
 }
-int conv_p_2_s(GFAKluge& gg)
+int conv_p_2_s(GFAKluge& gg, int g_t)
 {
 	//get all path elements
 	//if it is primary
@@ -166,6 +215,11 @@ int conv_p_2_s(GFAKluge& gg)
 	//length += 
 	//output partof_contigs
 	
+	if (g_t == 1) {
+		cout<<"H\tVN:Z:1.0"<<endl;
+	} else {
+		cout<<"H\tVN:Z:2.0"<<endl;
+	}
 	map<string, path_elem> pp = gg.get_name_to_path();		
 	map<string, vector<edge_elem>> n_2_e = gg.get_seq_to_edges();
 	map<string, sequence_elem, custom_key> n_2_s = gg.get_name_to_seq();
@@ -206,15 +260,15 @@ int conv_p_2_s(GFAKluge& gg)
 				s.length += cor_e - cor_s;
 				pre_seq_id = p.segment_names[i];
 			}
-			opt_elem o = {"LN","i",to_string(s.length)};
-			s.opt_fields.push_back(o); 
+			//opt_elem o = {"LN","i",to_string(s.length)};
+			//s.opt_fields.push_back(o); 
 			ss.push_back(s);
 			if (ss.size() > LIMITED_SIZE) 
-				output_seqs(ss);
+				output_seqs(ss, g_t);
 		}
 	}	
 	if (ss.size()) 
-		output_seqs(ss);
+		output_seqs(ss, g_t);
 	return NORMAL;
 }
 
@@ -266,21 +320,20 @@ int updated_gfa_sequences(GFAKluge &g, string f_n)
 int main(int argc, char *argv[])
 {
 
-	if (argc < 3) {
-		help();
-		return 1;
-	}
 	int c = 0;
-	optind = 1;
-	string ref_fl = 0; 
+	string ref_fl = ""; 
 	bool s_c = false;
-	while ((c = getopt(argc, argv, "r:sh")) < 0) {
+	int gfa_type = 1;
+	while ((c = getopt(argc, argv, "r:g:sh")) >= 0) {
 		switch(c) {
 			case 'r':
 				ref_fl = optarg;
 				break;
 			case 's':
 				s_c = true;
+				break;
+			case 'g':
+				gfa_type = atoi(optarg);
 				break;
 			case 'h':
 				help();
@@ -310,16 +363,16 @@ int main(int argc, char *argv[])
 		};
 	}	
 	if (check_seq(gg)) {
-		fprintf(stderr, "sequences error in gfa\n");	
+		fprintf(stderr, "length(seqeuence) != GFA Sequence Length Field\n");	
 		return LACK_KEY_INFO; 	
 	}
 	if (s_c) {
-		if (conv_p_2_s_s(gg)) {
+		if (conv_p_2_s_s(gg, gfa_type)) {
 			fprintf(stderr,"File format Error\n");
 			return FL_FORMAT_ERR;	
 		}	
 	}else {
-		if (conv_p_2_s(gg)) {
+		if (conv_p_2_s(gg, gfa_type)) {
 			fprintf(stderr,"File format Error\n");
 			return FL_FORMAT_ERR;	
 		}
